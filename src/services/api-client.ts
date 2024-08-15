1d0839973d4a3058641243c4a9c2d61511f04a9a
Anas Obaid <anasobaidpk@gmail.com>
2024-08-15 16:45:52 +0500

    Made key public

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+// const apiKey = import.meta.env.VITE_RAWG_API_KEY;
 
 export default axios.create({
   baseURL: "https://rawg-video-games-database.p.rapidapi.com",
   params: {
-    key: apiKey,
+    key: "1cb2bf18f39244e5945fbfd2701df8c5",
   },
   headers: {
     "x-rapidapi-key": "8d1e4d4a9fmshf2531737a3bc182p1be429jsnfaaf93c55128",
