3c982ae9653846a900fa1867e69f76f2be886f3d
Anas Obaid <anasobaidpk@gmail.com>
2024-08-15 16:31:32 +0500

    Using RapidAPI instead of rawg.io directly

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,8 +3,12 @@ import axios from "axios";
 const apiKey = import.meta.env.VITE_RAWG_API_KEY;
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
+  baseURL: "https://rawg-video-games-database.p.rapidapi.com",
   params: {
     key: apiKey,
+  },
+  headers: {
+    "x-rapidapi-key": "8d1e4d4a9fmshf2531737a3bc182p1be429jsnfaaf93c55128",
+    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
   },
 });
