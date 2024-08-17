d71cbc43314c74800c3c720ac89250d1e39ab5fe
Anas Obaid <anasobaidpk@gmail.com>
2024-08-17 18:20:39 +0500

    Improved security by hiding secrets

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 
-// const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+const baseURL = import.meta.env.VITE_RAPID_API_URL;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
+const rapidApiHost = import.meta.env.VITE_RAPID_API_HOST;
 
 export default axios.create({
-  baseURL: "https://rawg-video-games-database.p.rapidapi.com",
+  baseURL: baseURL,
   params: {
-    key: "1cb2bf18f39244e5945fbfd2701df8c5",
+    key: apiKey,
   },
   headers: {
-    "x-rapidapi-key": "8d1e4d4a9fmshf2531737a3bc182p1be429jsnfaaf93c55128",
-    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+    "x-rapidapi-key": rapidApiKey,
+    "x-rapidapi-host": rapidApiHost,
   },
 });
